be2a4a305795ef69feb235a425fde9e316d5b7be
shuheikurita <35558937+shuheikurita@users.noreply.github.com>
2025-12-09 11:08:02 +0000

    Deploying to gh-pages from @ nii-vla-lab/nii-vla-lab.github.io@e32dfaef1683f642025f055e901942604342485a 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "nii-vla-lab/nii-vla-lab",
+    "data-repo": "nii-vla-lab/nii-vla-lab.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
